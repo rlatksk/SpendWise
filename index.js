8438ca31712dd5b7bcef39239e3d0d8f4065c1c8
willsenyogi <willsenyp21@gmail.com>
2024-03-26 21:35:23 +0700

    attached dummy db schema

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const schema = require('./db-schema/dummySchema.js')
+app.use(express.json())
 
 dotenv.config()
 
 const port = process.env.PORT;
 
-app.use(express.json())
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to Database'));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
@@ -16,3 +19,11 @@ app.listen(port, () => {
 app.get('/', function(req,res){
     res.send('Test')
 })
+
+//Display stored data in db
+app.get('/db', async(req,res)=>{
+        const userData = await schema.find();
+        res.json(userData);
+})
+
+
