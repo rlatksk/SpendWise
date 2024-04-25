d44d1ab3ae0e2b314f89a933a7a5d4e5a51595a7
Justin Salim <justinsalim73@gmail.com>
2024-04-26 01:37:50 +0700

    Remove unused code

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const expressLayout = require('express-ejs-layouts');
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const dummySchema = require('./db-schema/dummySchema.js');
-// const dummyTransactionSchema = require('./db-schema/dummytransSchema.js');
-const realTransactionSchema = require('./db-schema/Transaction.js');
-const user = require('./db-schema/User.js');
 const authRoutes = require('./server/routes/auth');
 
 app.use('/', authRoutes);
@@ -39,70 +35,3 @@ app.get('/favicon.ico', (req, res) => res.status(204)); //Just to prevent chart.
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
-
-//CRUD FUNCTIONS for user schema (admin)
-function insertUserData (usernameValue, emailValue) {
-  usernameValue = usernameValue.toLowerCase();
-  if(usernameValue != null && emailValue != null){
-    dummySchema.insertOne(
-      {
-        username : usernameValue.toString(),
-        email : emailValue.toString(),
-      }
-    )
-  } else {
-    console.error('Username or email address is null')
-  }
-}
-
-function deleteUserData(usernameValue) {
-  usernameValue = usernameValue.toLowerCase();
-  if(usernameValue != null){
-    dummySchema.deleteMany({ username: usernameValue })
-    .then(() => console.log(`User data with username(${usernameValue.toString()}) deleted successfully`))
-    .catch((err) => console.error('Error deleting data:', err));
-  } else {
-    console.error('Username is null')
-  }
-}
-
-function updateUserEmail(usernameValue, oldEmailValue, emailValue){
-  usernameValue = usernameValue.toLowerCase();
-  if(usernameValue != null && oldEmailValue != null && emailValue != null){
-    dummySchema.updateOne(
-      { //old email verification
-      username : usernameValue,
-      email : oldEmailValue
-      }, 
-    
-      {email : emailValue})
-      .then(() => console.log(`Email address for username(${usernameValue}) has been updated to ${emailValue}`))
-      .catch((err) => console.error('Error deleting data:', err));
-  } else {
-    console.error('Email change failed ! (Cannot define old email address or new email address)');
-  }
-}
-
-
-//Display stored data in db
-app.get('/db', async(req,res)=>{
-  const userData = await dummySchema.find();
-  res.json(userData);
-})
-
-// app.get('/db/transactions', async(req,res)=>{
-//   const userData = await dummyTransactionSchema.find();
-//   res.json(userData);
-// })
-
-app.get('/db/realTransactions', async(req,res)=>{
-  const userData = await realTransactionSchema.find();
-  res.json(userData);
-})
-
-app.get('/db/user', async(req,res)=>{
-  const userData = await user.find();
-  res.json(userData);
-})
-
-  
