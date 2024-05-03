0c8d2ddef6ef38090c0c4aa0fd8e821ff13d0f56
ivander08 <ivanderseah08@gmail.com>
2024-05-03 20:56:14 +0700

    Fixed some bugs

--- db-schema/User.js
+++ db-schema/User.js
@@ -2,15 +2,29 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   id: String,
-  username: { type: String, unique: true, required: true},
-  email: { type: String, unique: true, required: true},
-  password: { type: String, required: true },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
   verificationCode: String,
-  verified: { type: Boolean, default: false },
+  verified: {
+    type: Boolean,
+    default: false
+  },
   resetPasswordToken: String,
   resetPasswordExpires: Date
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
